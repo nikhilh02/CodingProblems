3a39106c8bd5083b7aff5443e97ba5905d1ef8b2
nikhil harkangi <nh7302@rit.edu>
2025-01-28 23:45:42 -0500

    Adding comments

--- ArrayProductExceptSelf.java
+++ ArrayProductExceptSelf.java
@@ -1,3 +1,14 @@
+/*
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+ */
+
 public class ArrayProductExceptSelf {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
@@ -16,3 +27,8 @@ public class ArrayProductExceptSelf {
         return answer;
     }
 }
+
+/*
+Time complexity:O(n)
+Space complexity:O(1)
+ */
