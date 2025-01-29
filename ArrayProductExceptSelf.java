04e5a3960ff6961c8099d8823f7031e3c8470f30
nikhil harkangi <nh7302@rit.edu>
2025-01-28 23:43:43 -0500

    Product of Array Except Self Problem

---
+++ ArrayProductExceptSelf.java
@@ -0,0 +1,18 @@
+public class ArrayProductExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] answer = new int[n];
+
+        answer[0] = 1;
+        for(int i = 1; i < n; i++){
+            answer[i] = answer[i-1] * nums[i-1];
+        }
+
+        int postfix = 1;
+        for(int i = n-1; i >= 0; i--){
+            answer[i] = answer[i] * postfix;
+            postfix = postfix * nums[i];
+        }
+        return answer;
+    }
+}
