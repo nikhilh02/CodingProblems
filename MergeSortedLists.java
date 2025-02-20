40e24148ab5ede30c82f7e71b34c236bff8c0aaa
nikhil harkangi <nh7302@rit.edu>
2025-02-20 16:56:21 -0500

    Adding comments

--- MergeSortedLists.java
+++ MergeSortedLists.java
@@ -1,10 +1,12 @@
 public class MergeSortedLists {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
+        // Create a dummy node to simplify handling of the merged list
         ListNode dummy = new ListNode(0);
         ListNode node = dummy;
 
         while(list1!=null && list2!=null){
+            // Attach the smaller node to the merged list
             if(list1.val < list2.val){
                 node.next = list1;
                 list1 = list1.next;
@@ -12,14 +14,16 @@ public class MergeSortedLists {
                 node.next = list2;
                 list2 = list2.next;
             }
-            node = node.next;
+            node = node.next;       // Move to the newly added node
         }
 
+        // If any nodes remain in either list, attach them directly
         if(list1 != null)
             node.next = list1;
         else
             node.next = list2;
 
+        // Return the merged list starting from dummy.next (skip dummy node)
         return dummy.next;
     }
 }
