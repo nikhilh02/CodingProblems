baa31a880cbfd79f617dc22139ace9731bb3c90d
nikhil harkangi <nh7302@rit.edu>
2025-03-04 18:41:35 -0500

    Checking if string is valid palindrome

---
+++ Strings/Palindrome.java
@@ -0,0 +1,20 @@
+package Strings;
+
+//Two-pointer approach
+public class Palindrome {
+    public boolean isPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+        while(l < r){
+            while(l < r && !Character.isLetterOrDigit(s.charAt(l)))
+                l++;
+            while(l < r && !Character.isLetterOrDigit(s.charAt(r)))
+                r--;
+            if(Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r)))
+                return false;
+
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
