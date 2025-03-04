2abad9d05a1f1089face55db4f7ed52c8642ced4
nikhil harkangi <nh7302@rit.edu>
2025-03-04 18:43:52 -0500

    Adding comments

--- Strings/Palindrome.java
+++ Strings/Palindrome.java
@@ -5,6 +5,7 @@ public class Palindrome {
     public boolean isPalindrome(String s) {
         int l = 0, r = s.length() - 1;
         while(l < r){
+            // Move left pointer to the next alphanumeric character
             while(l < r && !Character.isLetterOrDigit(s.charAt(l)))
                 l++;
             while(l < r && !Character.isLetterOrDigit(s.charAt(r)))
@@ -12,9 +13,15 @@ public class Palindrome {
             if(Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r)))
                 return false;
 
+            // Move pointers towards the center
             l++;
             r--;
         }
         return true;
     }
 }
+
+/*
+Time Complexity: O(N)
+Space Complexity: O(1)
+ */
